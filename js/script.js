dfeaaad8424aa8e68fcd4a602257349da2569f62
larissacauane <larissa_cauane10@hotmail.com>
2022-04-26 21:29:57 -0300

    adiciona medias queries

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const faq = document.querySelectorAll(".faq");
 
-faq.forEach(element =>{
-    element.addEventListener("click", function(){
-        element.classList.toggle("active")
-    })
-})
+for (let index = 0; index < faq.length; index++) {
+  faq[index].addEventListener("click", function () {
+    faq[index].classList.toggle("active");
+  });
+}
